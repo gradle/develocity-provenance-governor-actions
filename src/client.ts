c20215209c468ef32ba4c325d65235a07c02214f
Ryan Nett <rnett@gradle.com>
2025-08-18 13:35:03 -0700

    Fix evaluator url

--- src/client.ts
+++ src/client.ts
@@ -180,7 +180,7 @@ class ApiClient implements Client {
     repositoryUrl: string
   ): Promise<PolicyResult> {
     const namespacePath = purl.namespace ? `/${purl.namespace}` : ''
-    const evalUrl = `/${tenant}/packages/${purl.type}${namespacePath}/${purl.name}/${purl.version}/policy-scans/${policyScan}`
+    const evalUrl = `${this.baseUrl}${tenant}/packages/${purl.type}${namespacePath}/${purl.name}/${purl.version}/policy-scans/${policyScan}`
 
     const payload = JSON.stringify({
       repositoryUrl: repositoryUrl,
