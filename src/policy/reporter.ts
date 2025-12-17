dc8aab444aafdec3f98ecb7d3ef6b87fd6279f97
Greg Cooper <gcooper@gradle.com>
2025-12-17 13:04:33 -0700

    Add logging

--- src/policy/reporter.ts
+++ src/policy/reporter.ts
@@ -124,6 +124,7 @@ function reportSubjectInfo(subject: PolicyRequestSubject) {
 }
 
 function attestationName(attestation: PolicyEvaluationResult): string {
+  core.info('Attestation: ' + JSON.stringify(attestation, null, 2))
   if (attestation.attestationDownloadUri) {
     const uri = attestation.attestationDownloadUri
     return uri.substring(uri.lastIndexOf('/') + 1)
