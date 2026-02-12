bfaad42c5dbbbc63bf6d7f83266925fe96b18067
Greg Cooper <gcooper@gradle.com>
2026-02-12 16:57:51 -0700

    Artifact path work

--- src/publish/reporter.ts
+++ src/publish/reporter.ts
@@ -105,12 +105,18 @@ function subjectInfo(
     const storeUri = 'https://' + repoUrlParts[0]
 
     let subjectPath = `${result.request.pkg.name}/${tag}`
+    let repository = repoUrlParts[1]
 
-    if (result && result.successes && result.successes[0]) {
-      subjectPath = `${result.successes[0].storeResponse.path}`
+    if (result && result.successes && result.successes[0]?.storeResponse) {
+      if (result.successes[0].storeResponse.path) {
+        subjectPath = result.successes[0].storeResponse.path
+      }
+      if (result.successes[0].storeResponse.repository) {
+        repository = result.successes[0].storeResponse.repository
+      }
     }
 
-    uiArtifactUri = `${storeUri}/ui/repos/tree/General/${repoUrlParts[1]}/${subjectPath}`
+    uiArtifactUri = `${storeUri}/ui/repos/tree/General/${repository}/${subjectPath}`
   }
 
   subjectSubHeader(
